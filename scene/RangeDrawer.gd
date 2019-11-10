90c2c4d24486ef3d9a3c391f61fbc32a472cb8e9
nalleyer <nalleyer@gmail.com>
2019-11-10 21:23:00 +0800

    fix(game): fix field draw bug

--- scene/RangeDrawer.gd
+++ scene/RangeDrawer.gd
@@ -5,7 +5,7 @@ func _ready():
 	mat = self.get_material()
 
 func update_field_r(new_r):
-	mat.set_shader_param("R", float(new_r) / Global.SIZE)
+	mat.set_shader_param("r", float(new_r) / Global.SIZE)
 
 
 func _process(delta):
