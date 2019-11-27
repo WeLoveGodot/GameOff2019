938373f92cd15d92124785b439423c86dc0aa358
HubertSun7 <345567848@qq.com>
2019-11-27 22:02:51 +0800

    feat: add the first version of MainGUI

--- scene/GUI/MainGUI.gd
+++ scene/GUI/MainGUI.gd
@@ -2,17 +2,11 @@ extends CanvasLayer
 
 # Declare member variables here. Examples:
 
-const delta_tech = 5
-
-
-
-const consumption_tech = 20
-
-# var b = "text"
-var _tech = 0
-var _energy = 0
-var _planet
-var _resource = 0
+# Game Variables
+var game
+var camera
+var explore
+var attack
 
 # GUI const values
 const BUTTON_EXPAND_DURATION = 0.2
@@ -27,28 +21,28 @@ onready var explore_button = get_node("MainGUIButtons/Explore")
 onready var attack_button = get_node("MainGUIButtons/Attack")
 onready var expand_button = get_node("MainGUIButtons/Expand")
 onready var progressor = get_node("Progressor")
-
-
+onready var player_health = get_node("PlayerInfo/HP/Num")
+onready var player_level = get_node("PlayerInfo/Level/Num")
+onready var player_tech = get_node("PlayerInfo/Tech/Num")
+onready var player_resource = get_node("PlayerInfo/Resource/Num")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(arrow)
 	menu_animator = get_node("Animator")
+	game = get_parent()
+	camera = game.get_node("Camera")
+	
 #  $ProgressBar.value = _tech
   #$Resource.text = String(_resource)
 
-func set_planet(planet):
-  _planet = planet
+func _process(delta):
+	if game.me:
+		#player_health.set_text(“ ” + String(game.me.energy))
+		player_level.set_text(" " + String(game.me.level))
+		player_tech.set_text(" " + String(game.me.tech))
+		player_resource.set_text(" " + String(game.me.energy))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
-func Upgrade(planet):
-  if planet.energy >= consumption_tech:
-    planet.energy -= consumption_tech
-    planet.tech += delta_tech
-    #$Resource.text = str(planet.resource)
-    #$ProgressBar.value = planet.tech
 
 
 func _on_Upgrade_pressed():
@@ -109,3 +103,22 @@ func _on_Attack_mouse_exited():
 							 attack_button.get_scale(), BUTTON_NORMAL_SCALE,\
 							 BUTTON_EXPAND_DURATION, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	menu_animator.start()
+
+func _on_Accelerate_pressed():
+	game.get_node("Skill").use_skill(
+		game.me,
+		Global.ESkill.ACC,
+		null
+	)
+
+
+func _on_Expand_pressed():
+	game.get_node("Skill").use_skill(game.me, Global.ESkill.EXPD, null)
+
+
+func _on_Explore_pressed():
+	camera.enter_draw_arrow_mode(Global.ESkill.EXPR, BUTTON_LEFT, null)
+
+
+func _on_Attack_pressed():
+	camera.enter_draw_arrow_mode(Global.ESkill.ATK, BUTTON_LEFT, game.me)
