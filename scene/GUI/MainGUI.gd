460c3265bcc659c17a1ee51b13217932d07c3fe6
HubertSun7 <345567848@qq.com>
2019-11-29 00:10:10 +0800

    fix: update the mainGUI

--- scene/GUI/MainGUI.gd
+++ scene/GUI/MainGUI.gd
@@ -21,10 +21,9 @@ onready var explore_button = get_node("MainGUIButtons/Explore")
 onready var attack_button = get_node("MainGUIButtons/Attack")
 onready var expand_button = get_node("MainGUIButtons/Expand")
 onready var progressor = get_node("Progressor")
-onready var player_health = get_node("PlayerInfo/HP/Num")
-onready var player_level = get_node("PlayerInfo/Level/Num")
-onready var player_tech = get_node("PlayerInfo/Tech/Num")
-onready var player_resource = get_node("PlayerInfo/Resource/Num")
+onready var player_level = get_node("Level/Num")
+onready var player_tech = get_node("Tech_Factor/Num")
+onready var player_resource = get_node("Resource/Num")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,17 +31,21 @@ func _ready():
 	menu_animator = get_node("Animator")
 	game = get_parent()
 	camera = game.get_node("Camera")
+	progressor.set_max(100)
 	
 #  $ProgressBar.value = _tech
   #$Resource.text = String(_resource)
 
 func _process(delta):
 	if game.me:
-		#player_health.set_text(“ ” + String(game.me.energy))
-		player_level.set_text(" " + String(game.me.level))
-		player_tech.set_text(" " + String(game.me.tech))
-		player_resource.set_text(" " + String(game.me.energy))
-
+		#player_health.set_text(“ENERGY : ” + String(game.me.energy))
+		player_level.set_text("LEVEL : " + String(game.me.level))
+		player_tech.set_text("GROTH RATE : " + String(game.me.tech_factor))
+		player_resource.set_text("ENERGY : " + String(game.me.energy))
+		print(str(game.me.tech) + " : " + str(game.me.progress))
+		progressor.set_value( game.me.progress * 100)
+	pass
+	
 
 
 func _on_Upgrade_pressed():
